7d8e9c39ed972ae6263d99a4b0cb5637ffddd0b6
Vineer <69795340+Vineer@users.noreply.github.com>
2022-05-28 00:31:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,48 @@
-import urllib.request
-#import pyth
-from time import sleep
-import telebot
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 
-bot = telebot.TeleBot('5303402669:AAHjGYKBF2GGPd_xSx-b3GxR2P65ruvPO78')
-  #sleep(0.5)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    url = "https://source.unsplash.com/random/?"+message.text
-    filename = "image.png"
-    urllib.request.urlretrieve(url, filename)
-    bot.send_photo(message.chat.id, open('image.png', 'rb'))
+class MainApp(App):
+    def build(self):
+        main_layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
+        self.solution = TextInput(multiline=False, readonly=False, halign="right", font_size=55)
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"],
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(text=label, pos_hint={"center_x": 0.5, "center_y": 0.5})
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
+
+        equals_button = Button(text="=", pos_hint={"center_x": 0.5, "center_y": 0.5})
+        equals_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equals_button)
+
+        # return a Button() as a root widget
+        return main_layout
+
+    def on_button_press(self, instance):
+        if instance.text == "C":
+            self.solution.text = ""
+        else:
+            self.solution.text += instance.text
+
+    def on_solution(self, instance):
+        if self.solution.text:
+            try:
+                self.solution.text = str(eval(self.solution.text))
+            except:
+                self.solution.text = "Error"
+
 
 if __name__ == '__main__':
-     bot.infinity_polling()
+    MainApp().run()
